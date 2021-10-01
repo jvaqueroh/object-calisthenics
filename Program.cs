c23e299c29e506a2a793a05c772cbaef9faeb50c
Juan V <jjsiete@gmail.com>
2021-10-01 23:59:30 +0200

    added initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace object_calisthenics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
