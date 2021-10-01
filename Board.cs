f7fa4fc9e28cdfe20773637adfde0f740ef8d709
Juan V <jjsiete@gmail.com>
2021-10-02 00:18:29 +0200

    Refactor

--- Board.cs
+++ Board.cs
@@ -5,12 +5,20 @@ public class Board{
 
     public string BuildBoard(){
         var boardBuilder = new StringBuilder();
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-                boardBuilder.Append(data[i,j]);                
-            }
-            boardBuilder.Append("\n");
-        }
+        CollectRows(boardBuilder);
         return boardBuilder.ToString();
+    }
+
+    private void CollectRows(StringBuilder boardBuilder){
+        for (int i = 0; i < 10; i++){
+            CollectSingleRow(boardBuilder, i);
+        }
+    }
+
+    private void CollectSingleRow(StringBuilder boardBuilder, int row){
+        for (int j = 0; j < 10; j++){
+            boardBuilder.Append(data[row, j]);
+        }
+        boardBuilder.Append("\n");
     }
 }
