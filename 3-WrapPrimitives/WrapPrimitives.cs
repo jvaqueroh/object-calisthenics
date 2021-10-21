1183d27ed68f6162b81874bab13e28a117b38c5d
Juan V <jjsiete@gmail.com>
2021-10-22 00:59:06 +0200

    Rule 3 - final example

--- 3-WrapPrimitives/WrapPrimitives.cs
+++ 3-WrapPrimitives/WrapPrimitives.cs
@@ -2,22 +2,47 @@ using System;
 
 public class WrapPrimitives{
     public void CreateAVehicle(){
-        var newVehicle = new Vehicle("A123", 1890);
-    }
+        var newVehicle = new Vehicle(
+            new SerialNumber("A123"), 
+            new ProductionYear(1890));
+    } 
 }
 
 public class Vehicle{
-    public string SerialNumber { get; private set; }
-    public int ProductionYear { get; private set; }
+    public SerialNumber SerialNumber { get; private set; }
+    public ProductionYear ProductionYear { get; private set; }
 
-    public Vehicle(string serialNumber, int productionYear)
+    public Vehicle(SerialNumber serialNumber, ProductionYear productionYear)
     {
-        if(string.IsNullOrWhiteSpace(serialNumber) || !serialNumber.StartsWith("W") || serialNumber.Length != 15)
-            throw new ArgumentException("serialNumber");
-        if(productionYear > DateTime.Now.Year || productionYear < 1900)
-            throw new ArgumentException("productionYear");
-        
         SerialNumber = serialNumber;
         ProductionYear = productionYear;
+    }
+}
+
+public class SerialNumber{
+    public SerialNumber(string value)
+    {
+        if (Validate(value))
+            throw new ArgumentException("value");
+    }
+
+    private bool Validate(string value)
+    {
+        return string.IsNullOrWhiteSpace(value) || !value.StartsWith("W") || value.Length != 15;
+    }
+
+    public string Value { get; }
+}
+
+public class ProductionYear{
+    public ProductionYear(int value)
+    {
+        if (Validate(value))
+            throw new ArgumentException("value");
+    }
+
+    private bool Validate(int value)
+    {
+        return value > DateTime.Now.Year || value < 1900;
     }
 }
