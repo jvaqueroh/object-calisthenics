761cbc42511b527665739314cd3b49a091b1f95d
Juan V <jjsiete@gmail.com>
2021-11-06 16:29:25 +0100

    Rule 4 - Initial example

---
+++ 4-CollectionsAsFirstOrderClasses/WrapCollections.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Order
+{
+    public List<Product> Products { get; private set; }
+    public Address ShipmentAddress { get; private set; }
+    public Customer Customer { get; private set; }
+
+    public void AddProduct(Product newProduct)
+    {
+        Product product = Products
+            .Where(product => product.SerialNumber.Equals(newProduct.SerialNumber))
+            .SingleOrDefault();
+
+        if (product == null)
+            product.Add(1);
+        else
+            Products.Add(newProduct);
+    }
+}
+
+public class Customer
+{
+}
+
+public class Address
+{
+}
+
+public class Product
+{
+    public string SerialNumber { get; private set; }
+    public int Quantity { get; private set; }
+
+    internal void Add(int v)
+    {
+        throw new NotImplementedException();
+    }
+}
