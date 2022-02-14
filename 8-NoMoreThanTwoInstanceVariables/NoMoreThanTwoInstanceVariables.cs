027a0429a20deeebd7bdcbf17584e423188eb0db
Juan V <jjsiete@gmail.com>
2022-02-14 23:17:33 +0100

    Added example for rule 8

---
+++ 8-NoMoreThanTwoInstanceVariables/NoMoreThanTwoInstanceVariables.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+public class OldName
+{
+    public string First { get; private set; }
+    public string Middle { get; private set; }
+    public string Last { get; private set; }
+}
+
+class Name
+{
+    public string Surname { get; private set; }
+    public GivenNames Given { get; private set; }
+}
+
+public class GivenNames
+{
+    public List<string> names { get; private set; }
+}
