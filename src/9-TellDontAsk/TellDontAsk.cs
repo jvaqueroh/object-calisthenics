b33a4af3019d814ee41a3e92ef9c62ca55bceec1
Juan V <jjsiete@gmail.com>
2022-02-19 00:24:59 +0100

    Simplify code. Make Main method executable. Remove Program.cs

--- src/9-TellDontAsk/TellDontAsk.cs
+++ src/9-TellDontAsk/TellDontAsk.cs
@@ -1,31 +1,29 @@
-using System;
+    using System;
 
-public class BankAccount {
-    private decimal balance;
-    private decimal creditLimit;
-    
-    public BankAccount(decimal balance, decimal creditLimit)
-    {
-        this.balance = balance;
-        this.creditLimit = creditLimit;
-    }
-    
-    public void WithdrawMoney(decimal moneyToWithdraw) {
-        if (balance < moneyToWithdraw || creditLimit < moneyToWithdraw)
-            throw new InvalidOperationException();
-        balance -= moneyToWithdraw;
+    public class BankAccount {
+        private decimal balance;
+        private decimal creditLimit;
+        
+        public BankAccount(decimal balance, decimal creditLimit) {
+            this.balance = balance;
+            this.creditLimit = creditLimit;
+        }
+        
+        public void WithdrawMoney(decimal moneyToWithdraw) {
+            if (balance < moneyToWithdraw || creditLimit < moneyToWithdraw)
+                throw new InvalidOperationException();
+            balance -= moneyToWithdraw;
+        }
+
+        public override string ToString() {
+            return $"Balance: {balance}, Credit limit: {creditLimit}";
+        }
     }
 
-    public override string ToString() {
-        return $"Balance: {balance}, Credit limit: {creditLimit}";
+    public static class Program {
+        public static void Main(string[] args) {
+            var myAccount = new BankAccount(1000M, 500M);
+            myAccount.WithdrawMoney(300M);
+            Console.WriteLine(myAccount);
+        }
     }
-}
-
-public class Program {
-    public void main() {
-        var myAccount = new BankAccount(1000M, 500M);
-        var moneyToWithdraw = 300M;
-        myAccount.WithdrawMoney(moneyToWithdraw);
-        Console.WriteLine(myAccount.ToString());
-    }
-}
